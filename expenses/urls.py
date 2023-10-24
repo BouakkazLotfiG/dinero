bdd0d7d4aa71a9e7be805233036385130ecd3623
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-10-24 12:03:14 +0100

    removed total

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import getExpenses, createExpense, deleteExpense, TotalExpense
+from .views import getExpenses, createExpense, deleteExpense
 
 urlpatterns = [
     path('', getExpenses.as_view(), name='expense-list'),
     path('create', createExpense.as_view(), name='expense-create'),
     path('delete/<int:pk>', deleteExpense.as_view(), name='expense-delete'),
-    path('total', TotalExpense.as_view(), name='expense-total'),
 ]
--- income/urls.py
+++ income/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import createIncome, deleteIncome, getIncome, TotalIncome
+from .views import createIncome, deleteIncome, getIncome
 
 urlpatterns = [
     path('', getIncome.as_view(), name='income-list'),
     path('create', createIncome.as_view(), name='income-create'),
     path('delete/<int:pk>', deleteIncome.as_view(), name='income-delete'),
-    path('total', TotalIncome.as_view(), name='income-total'),
 ]
