7fb54785cc4e5d2724881463a9b62215631d5ebb
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-10-24 09:39:06 +0100

    Fix: urls backslash

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,7 +3,7 @@ from .views import getExpenses, createExpense, deleteExpense, TotalExpense
 
 urlpatterns = [
     path('', getExpenses.as_view(), name='expense-list'),
-    path('create/', createExpense.as_view(), name='expense-create'),
+    path('create', createExpense.as_view(), name='expense-create'),
     path('delete/<int:pk>/', deleteExpense.as_view(), name='expense-delete'),
-    path('total/', TotalExpense.as_view(), name='expense-total'),
+    path('total', TotalExpense.as_view(), name='expense-total'),
 ]
--- income/urls.py
+++ income/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import createIncome, deleteIncome, getIncome, TotalIncome
 
 urlpatterns = [
-    # Other income-related URLs
     path('', getIncome.as_view(), name='income-list'),
-    path('create/', createIncome.as_view(), name='income-create'),
-    path('delete/<int:pk>/', deleteIncome.as_view(), name='income-delete'),
-    path('total/', TotalIncome.as_view(), name='income-total'),
+    path('create', createIncome.as_view(), name='income-create'),
+    path('delete/<int:pk>', deleteIncome.as_view(), name='income-delete'),
+    path('total', TotalIncome.as_view(), name='income-total'),
 ]
