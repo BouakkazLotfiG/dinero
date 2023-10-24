ced0d1fc9a672c4cebfda17bf69807d70facc8dd
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-10-24 10:20:58 +0100

    Fix: delete action added

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,6 +4,6 @@ from .views import getExpenses, createExpense, deleteExpense, TotalExpense
 urlpatterns = [
     path('', getExpenses.as_view(), name='expense-list'),
     path('create', createExpense.as_view(), name='expense-create'),
-    path('delete/<int:pk>/', deleteExpense.as_view(), name='expense-delete'),
+    path('delete/<int:pk>', deleteExpense.as_view(), name='expense-delete'),
     path('total', TotalExpense.as_view(), name='expense-total'),
 ]
